4503dfc600343792348d06e4d35b73b4d6fe1574
cmihk <null>
2024-12-02 04:50:18 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Blog Title :link: https://cmihk.github.io 
-### :page_facing_up: [2](https://cmihk.github.io/tag.html) 
+# DEMO :link: https://cmihk.github.io 
+### :page_facing_up: [0](https://cmihk.github.io/tag.html) 
 ### :speech_balloon: 0 
-### :hibiscus: 24 
-### :alarm_clock: 2024-12-02 02:25:15 
+### :hibiscus: 0 
+### :alarm_clock: 2024-12-02 12:50:18 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
---
+++ docs/js/theme.js
@@ -0,0 +1,146 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // 获取当前页面
+    let currentUrl = window.location.pathname;
+
+    // 主页的样式表
+
+    // 判断当前页面是否为主页
+    if (currentUrl == '/' || currentUrl.includes('/index.html') || currentUrl.includes('/page')) {
+        let style = document.createElement('style');
+        style.innerHTML = `
+
+          // 以下是原版样式
+          // 来源：templates/base.html
+          body {
+            box-sizing: border-box;
+            min-width: 200px;
+            max-width: 900px;
+            margin: 20px auto;
+            padding: 45px;
+            font-size: 16px;
+            font-family: sans-serif;
+            line-height: 1.25;
+          }
+
+          #header {
+            display: flex;
+            padding-bottom: 8px;
+            border-bottom: 1px solid var(--borderColor-muted, var(--color-border-muted));
+            margin-bottom: 16px;
+          }
+
+          #footer {
+            margin-top: 64px;
+            text-align: center;
+            font-size: small;
+          }
+
+          // 来源：templates/plist.html
+          .avatar {
+            transition: 0.8s;
+            width: 64px;
+            height: 64px;
+            object-fit: cover;
+          }
+
+          .avatar:hover {
+            transform: scale(1.15) rotate(360deg);
+          }
+
+          .title-left a {
+            color: inherit;
+            text-decoration: none;
+            vertical-align: bottom;
+            font-size: 40px;
+            font-weight: bold;
+            font-family: Monaco;
+            margin-left: 8px;
+          }
+
+          .title-right {
+            display: flex;
+            margin: auto 0 0 auto;
+          }
+
+          .title-right button {
+            margin-right: 8px;
+            padding: 16px;
+          }
+
+          .title-right .circle {
+            padding: 14px 16px;
+          }
+
+          .SideNav {
+            min-width: 360px;
+          }
+
+          .SideNav-icon {
+            margin-right: 16px;
+          }
+
+          .SideNav-item .Label {
+            color: #fff;
+            margin-left: 4px;
+          }
+
+          .d-flex {
+            min-width: 0;
+          }
+
+          .listTitle {
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            max-width: 100%;
+          }
+
+          .listLabels {
+            white-space: nowrap;
+            display: flex;
+          }
+
+          .listLabels object {
+            max-height: 16px;
+            max-width: 24px;
+          }
+
+          @media (max-width: 600px) {
+            body {
+              padding: 8px;
+            }
+
+            .avatar {
+              width: 40px;
+              height: 40px;
+            }
+
+            .blogTitle {
+              display: none;
+            }
+
+            #buttonRSS {
+              display: none;
+            }
+
+            .LabelTime {
+              display: none;
+            }
+          }
+        `;
+
+        // 添加样式表
+        document.head.appendChild(style);
+
+        // 搜索框回车触发搜索
+        let input = document.getElementsByClassName('form-control subnav-serch-input float-left')[0];
+        let button = document.getElementsByClassName('btn float-left')[0];
+        input.addEventListener('keyup', function (event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                button.click();
+            }
+        });
+    }
+})
