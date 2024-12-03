9f7c8b0f7b6d44b35b54065b92380cc2f5709d58
cmihk <null>
2024-12-03 13:47:33 +0000

    🎉auto update by Gmeek action

--- docs/js/theme.js
+++ docs/js/theme.js
@@ -1,146 +1,177 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
-    // 获取当前页面
-    let currentUrl = window.location.pathname;
+  // 获取所有style标签
+  let styleTags = document.querySelectorAll('style');
+  // 删除style标签
+  styleTags.forEach(tag => tag.remove());
 
-    // 主页的样式表
+  // 获取当前页面
+  let currentUrl = window.location.pathname;
 
-    // 判断当前页面是否为主页
-    if (currentUrl == '/' || currentUrl.includes('/index.html') || currentUrl.includes('/page')) {
-        let style = document.createElement('style');
-        style.innerHTML = `
+  // 主页的样式表
 
-          // 以下是原版样式
-          // 来源：templates/base.html
-          body {
-            box-sizing: border-box;
-            min-width: 200px;
-            max-width: 900px;
-            margin: 20px auto;
-            padding: 45px;
-            font-size: 16px;
-            font-family: sans-serif;
-            line-height: 1.25;
-          }
+  // 判断当前页面是否为主页
+  if (
+    currentUrl == '/' || 
+    currentUrl.includes('/index.html') || 
+    currentUrl.includes('/page')
+  ) {
+    let style = document.createElement('style');
+    style.innerHTML = `
 
-          #header {
-            display: flex;
-            padding-bottom: 8px;
-            border-bottom: 1px solid var(--borderColor-muted, var(--color-border-muted));
-            margin-bottom: 16px;
-          }
+      /* 以下是原版样式 */
+      /* 来源：templates/base.html */
+      body {
+        box-sizing: border-box;
+        min-width: 200px;
+        max-width: 900px;
+        margin: 20px auto;
+        padding: 45px;
+        font-size: 16px;
+        font-family: sans-serif;
+        line-height: 1.25;
+      }
 
-          #footer {
-            margin-top: 64px;
-            text-align: center;
-            font-size: small;
-          }
+      #header {
+        display: flex;
+        padding-bottom: 8px;
+        border-bottom: 1px solid var(--borderColor-muted, var(--color-border-muted));
+        margin-bottom: 16px;
+      }
 
-          // 来源：templates/plist.html
-          .avatar {
-            transition: 0.8s;
-            width: 64px;
-            height: 64px;
-            object-fit: cover;
-          }
+      #footer {
+        margin-top: 64px;
+        text-align: center;
+        font-size: small;
+      }
 
-          .avatar:hover {
-            transform: scale(1.15) rotate(360deg);
-          }
+      /* 来源：templates/plist.html */
+      .avatar {
+        transition: 0.8s;
+        width: 64px;
+        height: 64px;
+        object-fit: cover;
+      }
 
-          .title-left a {
-            color: inherit;
-            text-decoration: none;
-            vertical-align: bottom;
-            font-size: 40px;
-            font-weight: bold;
-            font-family: Monaco;
-            margin-left: 8px;
-          }
+      .avatar:hover {
+        transform: scale(1.15) rotate(360deg);
+      }
 
-          .title-right {
-            display: flex;
-            margin: auto 0 0 auto;
-          }
+      .title-left a {
+        color: inherit;
+        text-decoration: none;
+        vertical-align: bottom;
+        font-size: 40px;
+        font-weight: bold;
+        font-family: Monaco;
+        margin-left: 8px;
+      }
 
-          .title-right button {
-            margin-right: 8px;
-            padding: 16px;
-          }
+      .title-right {
+        display: flex;
+        margin: auto 0 0 auto;
+      }
 
-          .title-right .circle {
-            padding: 14px 16px;
-          }
+      .title-right button {
+        margin-right: 8px;
+        padding: 16px;
+      }
 
-          .SideNav {
-            min-width: 360px;
-          }
+      .title-right .circle {
+        padding: 14px 16px;
+      }
 
-          .SideNav-icon {
-            margin-right: 16px;
-          }
+      .SideNav {
+        min-width: 360px;
+      }
 
-          .SideNav-item .Label {
-            color: #fff;
-            margin-left: 4px;
-          }
+      .SideNav-icon {
+        margin-right: 16px
+      }
 
-          .d-flex {
-            min-width: 0;
-          }
+      .SideNav-item .Label {
+        color: #fff;
+        margin-left: 4px;
+      }
 
-          .listTitle {
-            overflow: hidden;
-            white-space: nowrap;
-            text-overflow: ellipsis;
-            max-width: 100%;
-          }
+      .d-flex {
+        min-width: 0;
+      }
 
-          .listLabels {
-            white-space: nowrap;
-            display: flex;
-          }
+      .listTitle {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        max-width: 100%;
+      }
 
-          .listLabels object {
-            max-height: 16px;
-            max-width: 24px;
-          }
+      .listLabels {
+        white-space: nowrap;
+        display: flex;
+      }
 
-          @media (max-width: 600px) {
-            body {
-              padding: 8px;
-            }
+      .listLabels object {
+        max-height: 16px;
+        max-width: 24px;
+      }
 
-            .avatar {
-              width: 40px;
-              height: 40px;
-            }
+      @media (max-width: 600px) {
+        body {
+          padding: 8px;
+        }
 
-            .blogTitle {
-              display: none;
-            }
+        .avatar {
+          width: 40px;
+          height: 40px;
+        }
 
-            #buttonRSS {
-              display: none;
-            }
+        .blogTitle {
+          display: none;
+        }
 
-            .LabelTime {
-              display: none;
-            }
-          }
-        `;
+        #buttonRSS {
+          display: none;
+        }
 
-        // 添加样式表
-        document.head.appendChild(style);
+        .LabelTime {
+          display: none;
+        }
+      }
+    `;
+    document.head.appendChild(style);
+  }
 
-        // 搜索框回车触发搜索
-        let input = document.getElementsByClassName('form-control subnav-serch-input float-left')[0];
-        let button = document.getElementsByClassName('btn float-left')[0];
-        input.addEventListener('keyup', function (event) {
-            event.preventDefault();
-            if (event.keyCode === 13) {
-                button.click();
-            }
-        });
-    }
+  // 文章页的样式表
+
+  // 判断当前页面是否为文章页
+  /*else if (
+    currentUrl.includes('/post/') || 
+    currentUrl.includes('/link.html') || 
+    currentUrl.includes('/about.html')
+  ) {
+    let style = document.createElement('style');
+    style.innerHTML = `
+    `;
+    document.head.appendChild(style);
+  }*/
+
+  // 搜索页的样式表
+
+  // 判断当前页面是否为搜索页
+  else if (currentUrl.includes('/tag')) {
+    /*let style = document.createElement('style');
+    style.innerHTML = `
+    `;
+    document.head.appendChild(style);*/
+
+    // 搜索框回车触发搜索
+    let input = document.getElementsByClassName('form-control subnav-search-input float-left')[0];
+    let button = document.getElementsByClassName('btn float-left')[0];
+    input.addEventListener('keyup', function(event) {
+      event.preventDefault();
+      if (event.keyCode === 13) {
+        button.click();
+      }
+    });
+  }
 })
