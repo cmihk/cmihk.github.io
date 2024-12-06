947196f69407599ce42c8fba8aba32278b013943
cmihk <null>
2024-12-06 18:37:42 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [2](https://cmihk.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 291 
-### :alarm_clock: 2024-12-07 02:15:58 
+### :alarm_clock: 2024-12-07 02:37:42 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/js/theme.js
+++ docs/js/theme.js
@@ -86,7 +86,59 @@ document.addEventListener('DOMContentLoaded', function() {
     currentUrl.includes('/about.html')
   ) {
     let style = document.createElement('style');
-    style.innerHTML = ``;
+    style.innerHTML = `
+
+      [data-color-mode=light][data-light-theme=dark_dimmed],
+      [data-color-mode=light][data-light-theme=dark_dimmed]::selection,
+      [data-color-mode=dark][data-dark-theme=dark_dimmed],
+      [data-color-mode=dark][data-dark-theme=dark_dimmed]::selection
+        --markdown-pre-bgColor: #27282d;
+      }
+
+      :root {
+        --markdown-pre-bgColor: #f6f8fa;
+      }
+
+      .markdown-body .highlight pre,
+      .markdown-body pre {
+        background-color: var(--markdown-pre-bgColor);
+      }
+
+      .clipboard-container {
+        opacity: 0;
+        visibility: hidden;
+        pointer-events: none;
+        transition: opacity 0.3s ease, visibility 0s 0.3s;
+        -webkit-transition: opacity 0.3s ease, visibility 0s 0.3s
+      }
+
+      .highlight:hover .clipboard-container {
+        opacity: 1;
+        visibility: visible;
+        pointer-events: auto;
+        transition: opacity 0.3s ease, visibility 0s 0s;
+        -webkit-transition: opacity 0.3s ease, visibility 0s 0s
+      }
+
+      [data-color-mode=light][data-light-theme=dark_dimmed],
+      [data-color-mode=light][data-light-theme=dark_dimmed]::selection,
+      [data-color-mode=dark][data-dark-theme=dark_dimmed],
+      [data-color-mode=dark][data-dark-theme=dark_dimmed]::selection
+        --color-accent-fg: #20d4ff;
+      }
+
+      a {
+        background: #90d1ff;
+        background: linear-gradient(#90d1ff, #90d1ff) no-repeat left bottom;
+        background-size: 0 2px;
+        transition: all 0.25s ease;
+        -webkit-transition: all 0.25s ease;
+      }
+
+      .markdown-body a:hover {
+        background-size: 100% 2px;
+      }
+    `;
 
     // 应用style样式
     document.head.appendChild(style);
